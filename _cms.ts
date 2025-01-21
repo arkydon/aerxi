7fae2a06e5353505820ffc8400a993960e9f795a
Arky <arky.lyn@gmail.com>
2025-01-21 18:17:48 +0530

    Added code to enable managing site with Luma CMS

--- _cms.ts
+++ _cms.ts
@@ -1,5 +1,91 @@
 import lumeCMS from "lume/cms/mod.ts";
+import { Field } from "lume/cms/types.ts";
 
-const cms = lumeCMS();
+const cms = lumeCMS({
+    extraHead: `
+  <style>
+    body {
+      font-family: "Barlow Condensed";
+    }
+  </style>
+    `,
+});
+
+
+
+cms.collection({
+    name:"Blog", 
+    store: "src:blog/**/*.md",
+    fields: [
+        "title: text",
+        {
+            name: "date",
+            type: "date",
+            init(field) {
+                field.value = new Date();
+            }
+        },
+        {
+            name: "tags",
+            label: "Tags",
+            type: "list",
+            value: ["blog"]
+        },
+        {   
+            name: "layout", 
+            type: "hidden", 
+            value: "layouts/post.vto"
+            /* init(field) {field.value = "layouts/post.vto";} */
+,        },
+        {
+            name: "og_description",
+            label: "Open Graph Description",
+            type: "text",
+        },
+        {
+            name: "content",
+            label: "Blog Post Content",
+            type: "markdown"
+        },
+    ],
+    documentName: "{title}.md",
+});
+cms.collection("Hub", "src:hub/**/*.md", [
+    "title: text",
+    "date: date",
+    "tags: list",
+    "content: markdown",
+]);
+
+cms.document("Changelog", "src:hub/notes/changelog/index.vto", [
+    "content: markdown",
+]);
+
+cms.document("Now", "src:hub/notes/now/index.md", [
+    "content: markdown",
+]);
+
+cms.document("Blogroll", "src:hub/web/blogroll/blog-roll.vto", [
+    "content: markdown",
+]);
+
+cms.collection({ 
+    name: "Favourites", 
+    store: "src:hub/notes/about/_data/favourites/*.yaml", 
+    fields: [
+        {
+            name: "title",
+            type: "text",
+        },
+        {
+            name: "data",
+            type: "list",
+        }
+]});
+
+cms.upload(
+    "uploads: Uploaded files",
+    "src:**/*{.jpg,.svg,.png,.gif,.mp3,.mp4,.pdf,.webp}",
+);
 
 export default cms;
