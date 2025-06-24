dd3b2e5e781afecb54c27031ebda45ec7689ae8b
Arky <arky.lyn@gmail.com>
2025-06-24 13:26:59 +0530

    Changed heading font to Archivo using the google fonts plugin

--- _config.ts
+++ _config.ts
@@ -58,4 +58,17 @@ site.use(googleFonts({
 }));
 */
 
+site.use(googleFonts({
+    cssFile: "static/styles.css",
+    placeholder: "/* google-fonts */",
+      subsets: [
+    "latin",
+    "latin-ext",
+    ],
+    fonts:
+        {
+            Arch: "https://fonts.googleapis.com/css2?family=Archivo:ital,wdth,wght@0,62.5,100..900;1,62.5,100..900&display=swap",
+        }     
+}));
+
 export default site;
