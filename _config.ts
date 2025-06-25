34c514aa9772ad3685f218df570c88b776091ff9
Arky <arky.lyn@gmail.com>
2025-06-25 18:26:28 +0530

    Added card design at the top

--- _config.ts
+++ _config.ts
@@ -68,7 +68,8 @@ site.use(googleFonts({
     fonts:
         {
             Arch: "https://fonts.googleapis.com/css2?family=Archivo:ital,wdth,wght@0,62.5,100..900;1,62.5,100..900&display=swap",
-            headtitlefont: "https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&display=swap"
+            headtitlefont: "https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&display=swap",
+            signfont: "https://fonts.googleapis.com/css2?family=Alegreya+Sans+SC:wght@400;500&display=swap",
         }     
 }));
 
